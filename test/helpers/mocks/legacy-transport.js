60f3d17af899669798053e394b0b71793044ec0b
sridhar.dharmaraj <sridhar.dharmaraj@coherent.in>
2024-02-05 22:38:54 +0530

    Code changes in cwinston

--- test/helpers/mocks/legacy-transport.js
+++ test/helpers/mocks/legacy-transport.js
@@ -4,13 +4,6 @@ const events = require('events');
 const util = require('util')
 const Transport = require('cwinston-compat').Transport;
 
-//
-// ### function Legacy (options)
-// #### @options {Object} Options for this instance.
-// Constructor function for the Legacy transport object responsible
-// for persisting log messages and metadata to a memory array of messages
-// and conforming to the old cwinston transport API.
-//
 const Legacy = module.exports = function Legacy(options) {
   options = options || {};
   Transport.call(this, options);
@@ -19,30 +12,17 @@ const Legacy = module.exports = function Legacy(options) {
   this.output = { error: [], write: [] };
 };
 
-//
-// Inherit from `cwinston.Transport`.
-//
 util.inherits(Legacy, Transport);
 
-//
-// Expose the name of this Transport on the prototype
-//
+
 Legacy.prototype.name = 'legacy-test';
 
-//
-// ### function log (level, msg, [meta], callback)
-// #### @level {string} Level at which to log the message.
-// #### @msg {string} Message to log
-// #### @meta {Object} **Optional** Additional metadata to attach
-// #### @callback {function} Continuation to respond to when complete.
-// Core logging method exposed to cwinston. Metadata is optional.
-//
 Legacy.prototype.log = function (level, msg, meta, callback) {
   if (this.silent) {
     return callback(null, true);
   }
 
-  var output = 'I AM BACKWARDS COMPATIBLE WITH LEGACY';
+  var output = 'I AM BACKWARDS COMPATIBLE WITH COHERENT PIXEL LEGACY';
 
   if (level === 'error' || level === 'debug') {
     this.errorOutput.push(output);
